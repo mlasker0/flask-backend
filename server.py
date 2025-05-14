794135c9b358d0a25d36767208b61ef346e01337
mlasker0 <shantulasker30@gmail.com>
2025-05-14 14:02:00 +0600

    fix: added port binding for render

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+authors = []
+
+@app.route('/add_author', methods=['POST'])
+def add_author():
+    data = request.json
+    authors.append(data['name'])
+    return jsonify({"message": "Author added successfully"})
+
+@app.route('/get_authors', methods=['GET'])
+def get_authors():
+    return jsonify(authors)
+
+if __name__ == '__main__':
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
